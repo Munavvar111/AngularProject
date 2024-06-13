f5fdc4757d1f3cb813e5b0a3cc6a6a7448953e2a
Munavvar Popatiya <tatva.dotnet.munavvarpopatiya@outlook.com>
2024-06-13 13:58:04 +0530

    chagnes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { CountrtPageComponent } from './country-page/country-page.component';
 import { WeatherComponent } from './weather/weather.component';
 
 export const routes: Routes = [
     {path:"",component:LandingPageComponent},
-    {path:"countrtList",component:CountrtPageComponent},
     { path: 'weather/:latitude/:longitude', component: WeatherComponent }
 
 ];
